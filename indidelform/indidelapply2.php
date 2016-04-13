0a63d267484e1261a55e8d29367c69d9dc3fc96a
SwG Ghosh <codecrafts.cf@icloud.com>
2016-04-14 00:23:46 +0530

    Added logo.png and changed app success in indidelapply2

--- indidelform/indidelapply2.php
+++ indidelform/indidelapply2.php
@@ -71,7 +71,7 @@ if(mysqli_query($db, $sql) == false) {
       
         <h1><br/><a href="http://register.dpsnmunc.in"><img src="../cred.png" width="100%"/></a><br/>Individual Delegate<br/>Application</h1>
         <br/>
-        Application Successful.
+        Welcome aboard! See you at the conference.
       </form>
       <br/>
       <footer>
