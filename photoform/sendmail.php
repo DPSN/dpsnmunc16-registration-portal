c4eefdb91387034f90f605d42481733ef32d4bbf
SwG Ghosh <codecrafts.cf@icloud.com>
2016-04-24 12:28:12 +0530

    Change in auto email message

--- photoform/sendmail.php
+++ photoform/sendmail.php
@@ -1,6 +1,6 @@
 <?php
 $subject = "Welcome aboard! DPSNMUNC'16 Photography and Videography Application";
-$message = "Thank you for applying at DPSNMUNC '16. \r\nYour application has been selected. You will be intimated on the deadline for the payment of the delegation fees which has been kept at INR : 1,000 only. See you on the 30th of June , 1st and 2nd of July.
+$message = "Thank you for applying at DPSNMUNC '16. \r\nYour application has been selected. You will be intimated on the deadline for the payment of the delegation fees which has been kept at INR : 1,000 only. See you on the 30th of June, 1st and 2nd of July.
 \r\nThe Secretariat,\r\nDelhi Public School Newtown Model United Nations Conference 2016.\r\ncontact@dpsnmunc.in";
 $headers = 'From: contact@dpsnmunc.in' . "\r\n" .
     'Reply-To: contact@dpsnmunc.in' . "\r\n" .
